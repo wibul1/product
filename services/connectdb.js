ec5750cb60ffd760c2d9703c612b62105fbecde8
wibul1 <wibul.s@psru.ac.th>
2024-09-11 15:22:22 +0700

    edit connent mongodb

--- services/connectdb.js
+++ services/connectdb.js
@@ -1,12 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();  // โหลด environment variables จาก .env
 import mongoose from 'mongoose';
 
 const { MONGO_URI } = process.env;
 
+// console.log(MONGO_URI);
 export const connectToDb = async () => {
   try {
     await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true, 
-      useUnifiedTopology: true,
+      // useNewUrlParser: true, 
+      // useUnifiedTopology: true,
     });
     console.log('Connected to MongoDB');
   } catch (error) {
